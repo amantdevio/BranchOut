54588b984b6e6347a5cdfef2bf060e0bc227eafa
Aman Tiwari <amantdevio@gmail.com>
2026-01-13 03:06:54 +0530

    Fix: Fixed the API Routes

--- server.js
+++ server.js
@@ -18,9 +18,8 @@ const io = new Server(server, {
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-app.use(express.static("public"));
-
 app.use('/api/auth',userRoutes);
+app.use(express.static("public"));
 
 handleSocket(io)
 
